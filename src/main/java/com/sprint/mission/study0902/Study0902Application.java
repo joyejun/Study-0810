60a06cece75a816b33ef0450d17b44e638bec754
joyejun <joyejun000@gmail.com>
2026-09-02 11:11:00 +0900

    feat: Initial commit

---
+++ src/main/java/com/sprint/mission/study0902/Study0902Application.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.study0902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0902Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study0902Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/mission/study0902/Study0902ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.study0902;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study0902ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
