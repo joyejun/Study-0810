2a5c195eca267327a9d855d1396d3ce55eee97d3
joyejun <joyejun000@gmail.com>
2026-09-02 11:20:00 +0900

    postgresql 데이터베이스 내 table 생성 <- jpa 자동생성을 활용하여 user, table 엔티티 테이블 생성

---
+++ src/main/java/com/sprint/mission/study0902/Message.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.study0902;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //id 생성 책임을 database 에 위임한다.
+    private Integer id;
+    private String message;
+    private Integer userId;
+    private LocalDateTime createAt;
+
+}
---
+++ src/main/java/com/sprint/mission/study0902/User.java
@@ -0,0 +1,29 @@
+package com.sprint.mission.study0902;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)  //id 생성 책임을 database 에 위임한다.
+        private Integer id;
+        private String  name;
+        private Integer age;
+        private String job;
+        private String specialty;
+        private LocalDateTime createAt;
+
+}
